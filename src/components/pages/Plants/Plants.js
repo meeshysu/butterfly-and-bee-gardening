2b70264373d045c0ba4de6575aba693eb6d4e99a
Michelle Beshears <meeshysu@gmail.com>
2019-01-23 20:52:58 -0600

    working on the plants to show

--- src/components/pages/Plants/Plants.js
+++ src/components/pages/Plants/Plants.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import connection from '../../../helpers/data/connection';
+import plantRequest from '../../../helpers/data/plantRequest';
 import './Plants.scss';
 
+
 class Plants extends React.Component {
+  state = {
+    plants: [],
+  }
+
+  componentDidMount() {
+    connection();
+    plantRequest.getRequest()
+      .then((plants) => {
+        this.setState({ plants });
+      })
+      .catch(err => console.error('error with componentDidMount', err));
+  }
+
   render() {
     return (
       <div className='plantsPage mx-auto'>
-    <h3>Feast yo eyes on dese plants</h3>
+        <h3>Feast yo eyes on dese plants</h3>
       </div>
     );
   }
--- src/helpers/data/plantRequest.js
+++ src/helpers/data/plantRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import apiKeys from '../apiKeys';
+import apiKeys from './apiKeys';
 
 const firebaseUrl = apiKeys.firebaseConfig.databaseURL;
 
