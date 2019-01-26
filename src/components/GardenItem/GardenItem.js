a0cad21e8ddacc4f203e12a5e9582a3f5ce701d8
Michelle Beshears <meeshysu@gmail.com>
2019-01-26 12:15:06 -0600

    buttons for delete and add printed

--- src/components/GardenItem/GardenItem.js
+++ src/components/GardenItem/GardenItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Button } from 'reactstrap';
 import gardenShape from '../../helpers/propz/gardenShape';
 import './GardenItem.scss';
 
@@ -12,6 +13,7 @@ class GardenItem extends React.Component {
     return (
       <span className='row'>
         <span className='card-body'>
+          <pre className='delete-add-button btn-btn'><i class="far fa-minus-square"></i>   <i class="far fa-plus-square"></i></pre>
           <p className='garden-name'>{garden.name}</p>
           <p className='garden.description'>{garden.description}</p>
         </span>
