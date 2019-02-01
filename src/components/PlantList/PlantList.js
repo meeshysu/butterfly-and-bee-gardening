d9cae91e3464015ccec6554f5f2c55273da2ca69
Michelle Beshears <meeshysu@gmail.com>
2019-02-01 15:41:59 -0600

    search console logs

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import SearchField from 'react-search-field';
 import PlantItem from '../PlantItem/PlantItem';
 import plantShape from '../../helpers/propz/plantShape';
 import './PlantList.scss';
@@ -10,20 +9,6 @@ class PlantList extends React.Component {
     plants: PropTypes.arrayOf(plantShape),
     gardenId: PropTypes.string,
   }
-
-  // onChange = (value, e) => {
-  //   const { plants } = this.state;
-  //   filteredPlants = [],
-  //     e.preventDefault();
-  //   if (!value) {
-  //     this.setState({ filteredPlants: plants });
-  //   } else {
-  //     plants.forEach((plant) => {
-  //       if (plant.name.toLowerCase().includes(value.toLowerCase())
-  //         || plant.size.toLowerCase().includes(value.toLowerCase())
-  //         || plant.sun.toLowerCase().includes(value.toLowerCase())
-  //       ))};
-  // }
 
   render() {
     const {
@@ -40,12 +25,6 @@ class PlantList extends React.Component {
     return (
       <span className='col'>
         <h2>Plants</h2>
-        {/* <SearchField
-          placeholder="Search..."
-          onChange={this.onChange}
-          searchText="This is initial search text"
-          classNames="test-class"
-        /> */}
         <li>{plantItemComponent}</li>
       </span>
     );
--- src/components/pages/Plants/Plants.js
+++ src/components/pages/Plants/Plants.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import plantRequest from '../../../helpers/data/plantRequest';
-import myPlantsRequests from '../../../helpers/data/myPlantsRequests';
+import SearchField from 'react-search-field';
 import PlantList from '../../PlantList/PlantList';
 import './Plants.scss';
 
@@ -14,27 +14,42 @@ class Plants extends React.Component {
     plantRequest.getPlantRequest()
       .then((plants) => {
         this.setState({ plants });
+        this.setState({ filteredPlants: plants });
       })
       .catch(err => console.error('error with componentDidMount in plants', err));
   }
 
-  // goodbyeMyPlants = (myPlantId) => {
-  //   myPlantsRequests.deleteMyPlant(myPlantId)
-  //     // .then(() => {
-  //     //   myPlantsRequests.getMyPlantsByGardenId()
-  //     //     .then((myPlants) => {
-  //     //       this.setState({ myPlants });
-  //     //     });
-  //     // })
-  //     .catch(err => console.error('error with goodbyeMyPlants', err));
-  // }
+  onChange = (value, e) => {
+    console.log(value);
+    const { plants } = this.state;
+    const filteredPlants = [];
+    e.preventDefault();
+    if (!value) {
+      this.setState({ filteredPlants: plants });
+    } else {
+      plants.forEach((plant) => {
+        if (plant.name.toLowerCase().includes(value.toLowerCase()) || plant.color.toLowerCase().includes(value.toLowerCase())) {
+          filteredPlants.push(plants);
+        }
+        console.log(filteredPlants);
+        this.setState({ filteredPlants });
+      });
+    }
+  }
 
   render() {
-    const { plants } = this.state;
+    const { plants, filteredPlants } = this.state;
     return (
       <div className='plantsPage mx-auto'>
+        <SearchField
+          placeholder="Search Beans..."
+          onChange={this.onChange}
+          searchText=""
+          classNames="test-class w-100"
+        />
         <PlantList plants={plants}
-        goodbyeMyPlants={this.goodbyeMyPlants} />
+          goodbyeMyPlants={this.goodbyeMyPlants}
+          filteredPlants={filteredPlants} />
       </div>
     );
   }
