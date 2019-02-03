877210f2df492b10fab130272e725bcd2723fbae
Michelle Beshears <meeshysu@gmail.com>
2019-02-03 17:03:02 -0600

    saving, have to shut down for reload of gimp

--- src/components/GardenList/GardenList.js
+++ src/components/GardenList/GardenList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './GardenList.scss';
+// import './GardenList.scss';
 import gardenShape from '../../helpers/propz/gardenShape';
 import GardenItem from '../GardenItem/GardenItem';
 
