dcd7296f51a18d1cc6a60832f8882d2b5a198332
Michelle Beshears <meeshysu@gmail.com>
2019-02-09 11:21:56 -0600

    okay this time it actually fixed the myPlant deletion. i lied last time.

--- src/components/GardenItem/GardenItem.js
+++ src/components/GardenItem/GardenItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import gardenRequest from '../../helpers/data/gardenRequest';
+// import myPlantRequest from '../../helpers/data/myPlantsRequests';
+// import smashRequests from '../../helpers/data/smashRequests';
 import gardenShape from '../../helpers/propz/gardenShape';
-// import authRequests from '../../helpers/data/authRequests';
+import myPlantsRequest from '../../helpers/data/myPlantsRequests';
 import './GardenItem.scss';
 
 class GardenItem extends React.Component {
@@ -17,13 +18,14 @@ class GardenItem extends React.Component {
   deleteGardenEvent = (e) => {
     e.preventDefault();
     const { deleteSingleGarden, garden } = this.props;
+    myPlantsRequest.getMyPlantsByGardenId(garden.id)
+      .then((myPlants) => {
+        myPlants.forEach((plant) => {
+          myPlantsRequest.deleteMyPlant(plant.id);
+        });
+      })
+      .catch(err => console.error('error in deletegardenevent', err));
     deleteSingleGarden(garden.id);
-  }
-
-  deleteMyPlantsFromGarden = (e) => {
-    e.preventDefault();
-    const { getMyPlantsPlants, deleteSingleGarden } = this.props;
-    getMyPlantsPlants(deleteSingleGarden.id);
   }
 
   editGardenEvent = (e) => {
@@ -42,25 +44,25 @@ class GardenItem extends React.Component {
     const { garden } = this.props;
     return (
       <div className='gardensPageContainer'>
-          <ul className='row'>
-            <ul className='container row'>
-              <span className='card-body gardenCard' id={garden.id}>
-                <p className='garden-name'>{garden.name}</p>
-                <p className='garden-description'>{garden.description}</p>
-              </span>
-            </ul>
-              <span className='gardenButtons'>
-                <span className='delete-button btn-btn'>
-                  <i className="far fa-minus-square fa-2x" onClick={this.deleteGardenEvent}></i>
-                </span>
-                <span className='edit-button btn-btn'>
-                  <i className="fas fa-pen-square fa-2x" onClick={this.editGardenEvent}></i>
-                </span>
-                <span className='garden-detail btn-btn fa-2x' onClick={this.gardenDetailView}>
-                  <i className="fas fa-angle-double-right"></i>
-                </span>
-              </span>
-            </ul>
+        <ul className='row'>
+          <ul className='container row'>
+            <span className='card-body gardenCard' id={garden.id}>
+              <p className='garden-name'>{garden.name}</p>
+              <p className='garden-description'>{garden.description}</p>
+            </span>
+          </ul>
+          <span className='gardenButtons'>
+            <span className='delete-button btn-btn'>
+              <i className="far fa-minus-square fa-2x" onClick={this.deleteGardenEvent}></i>
+            </span>
+            <span className='edit-button btn-btn'>
+              <i className="fas fa-pen-square fa-2x" onClick={this.editGardenEvent}></i>
+            </span>
+            <span className='garden-detail btn-btn fa-2x' onClick={this.gardenDetailView}>
+              <i className="fas fa-angle-double-right"></i>
+            </span>
+          </span>
+        </ul>
       </div>
     );
   }
